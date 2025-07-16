32b627a8f96b5250fc868a796e0a2b928ff39b63
Lan-c <stataf.dev@gmail.com>
2025-07-16 10:54:08 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const quotes = [
+  "The best way to get started is to quit talking and begin doing.",
+  "Code is like humor. When you have to explain it, it’s bad.",
+  "First, solve the problem. Then, write the code.",
+  "Don’t watch the clock; do what it does. Keep going.",
+  "Experience is the name everyone gives to their mistakes.",
+];
+
+function showQuote() {
+  const quoteEl = document.getElementById("quote");
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  quoteEl.textContent = quotes[randomIndex];
+}
